f20c6e4306df6c268c59670ca659f20191257b41
simonwzing <simon_wzing@hotmail.com>
2017-01-08 22:59:14 +0800

    knn code running

--- mlrampup/supervisedlearning/classifier/knn.py
+++ mlrampup/supervisedlearning/classifier/knn.py
@@ -1,6 +1,7 @@
 from numpy import *
 import operator
 
+
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
@@ -12,7 +13,13 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteILabel = labels[sortedDistIndices[i]]
         classCount[voteILabel] = classCount.get(voteILabel,0)+1
-    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1),  reverse = True)
+
+    error = ''
+    try:
+        sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1),  reverse = True)
+    except Exception as e:
+        error= str(e)
+
     return sortedClassCount[0][0]
 
 
