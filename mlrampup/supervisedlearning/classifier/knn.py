c5ac000371cf9f08edfff80bbb2881ac496b292c
simonwzing <simon_wzing@hotmail.com>
2017-01-08 22:19:42 +0800

    knn code finished

--- mlrampup/supervisedlearning/classifier/knn.py
+++ mlrampup/supervisedlearning/classifier/knn.py
@@ -3,5 +3,19 @@ import operator
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistance = sqDiffMat.sum(axis = 1)
+    distances = sqDistance ** 0.5
+    sortedDistIndices = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteILabel = labels[sortedDistIndices[i]]
+        classCount[voteILabel] = classCount.get(voteILabel,0)+1
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1),  reverse = True)
+    return sortedClassCount[0][0]
+
+
+
     
 
